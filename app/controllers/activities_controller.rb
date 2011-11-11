37ea033616fe3c6ba3844bcf489fc6530c3732e1
Elena Nazzi <elna@itu.dk>
2011-11-12 00:00:22 +0100

    authentication for activities

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,7 @@
 class ActivitiesController < ActionController::Base
+  before_filter :authenticate
  
+ # get all the aspects names
   def index
     @response = JSON.parse(current_user.access_token.token.get('/api/v0/aspects'))
     @activities = @response
@@ -12,13 +14,13 @@ class ActivitiesController < ActionController::Base
   
   # GET activities/:id reply to 
   def show
-    @data1 = params[:id] 
+    @data1 = params[:id] # id is the name
     @response = JSON.parse(current_user.access_token.token.get('/api/v0/aspect_posts?aspect_name='+params[:id]))
     @contacts = JSON.parse(current_user.access_token.token.get('/api/v0/aspects/'+params[:id]+'/contacts'))
     
     respond_to do |format|
       format.html
-      format.json {render json: @response}
+      format.json {render json: {"response"=>@response,"contacts"=>@contacts}}
     end
   end
   
@@ -61,5 +63,30 @@ class ActivitiesController < ActionController::Base
       format.json {render json: @activitycounter}
     end
   end
+  
+  # before filter method to check on the identity of the user. So far it is very bad and no password is provided 
+  # BUT 
+  # we need some proper authentication methods
+  # it was suggested to use NFC with a private key as a signature for the objects. In the case of tablets it can be the user signature
+  #
+  def authenticate
+    case request.format
+    when Mime::XML, Mime::JSON #authentication only applies for these types of requests
+       if (params[:user]!=nil )
+         user = User.find_by_diaspora_id(params[:user])  
+         if(user!=nil)
+           request.env["warden"].set_user(user, :scope => :user, :store => true)
+           @answer =  current_user.access_token
+          else
+           @answer = '401 Unauthorized - This user is not registered, please register it first on your Diaspora Client service'
+          end
+       else
+         @answer='400 Bad Request - You need to send the user name with the domain of diaspora'
+       end
+      respond_to do |format|
+        format.xml {render xml: @answer}
+        format.json {render json: @answer}
+  end
+  
   
 end
