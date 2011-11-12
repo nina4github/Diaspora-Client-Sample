b261fd9de9df23679e9cdd458ccfa7eea33cd41c
Elena Nazzi <elna@itu.dk>
2011-11-12 01:27:27 +0100

    fixed authentication redirect

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -92,7 +92,7 @@ class ActivitiesController < ActionController::Base
       #         format.json {render json: @answer}
       #       end
     else
-      redirect_to("/") and return false
+      redirect_to("#{Rails.root}") and return false
     end
   end
   
