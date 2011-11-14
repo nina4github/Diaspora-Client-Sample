01ce20f67b3b26956bf767064319cf9a9539d8e4
Elena Nazzi <elna@itu.dk>
2011-11-14 15:20:23 +0100

    introduce profiles method and refined contacts?

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -111,8 +111,8 @@ class ActivitiesController < ActionController::Base
     @response = JSON.parse(current_user.access_token.token.get('/api/v0/aspects/'+params[:activityname]+'/contacts'))
     contact_ids=[]
     j=0
-    @response['contacts'][0].each do |contact|
-           contact_ids[j] = contact.person_id
+    @response['contacts'].each do |contact|
+           contact_ids[j] = contact['person_id']
            j=j+1
     end
     
