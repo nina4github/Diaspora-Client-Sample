65c6a2cd8dfbc2e083463722458819d07bea5c22
Elena Nazzi <elna@itu.dk>
2011-11-28 00:08:04 +0100

    upload fix controller for using diaspora

--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -11,7 +11,7 @@ end
 # Handle POST-request (Receive and save the uploaded file)
 def create
   params[:activity]
-  @response = JSON.parse(current_user.access_token.token.post('/api/v0/aspects/'+params[:activity]+'/upload', params['myfile']))
+  @response = JSON.parse(@current_user.access_token.token.post('/api/v0/aspects/'+params[:activity]+'/upload', params['myfile']))
 
   File.open('public/images/' + params['myfile'].original_filename, "wb") do |f|
     f.write(params['myfile'].tempfile.read)
