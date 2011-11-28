9b55f5ef221e868ae8e45d7a07c06de7f7ebfdab
Elena Nazzi <elna@itu.dk>
2011-11-28 18:22:38 +0100

    upload fix controller for using diaspora

--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -13,16 +13,19 @@ def create
   params[:activity]='shopping'
   user = User.find_by_diaspora_id('communityawvej@idea.itu.dk:3000')
   request.env["warden"].set_user(user, :scope => :user, :store => true)
-  message = Hash.new
-  message = {'myfile'=>
-                  { 'original_filename'=>params['myfile'].original_filename,
-                     'tempfile' =>File.new(params['myfile'].tempfile)
-                     }
-              } 
+  message = {
+    :original_filename => params['myfile'].original_filename,
+    :file => params['myfile'].tempfile
+  }
+  # message = {'myfile'=>
+  #                 { 'original_filename'=>params['myfile'].original_filename,
+  #                    'tempfile' =>File.new(params['myfile'].tempfile)
+  #                    }
+  #             } 
       
       #payload = { :myfile => Faraday::UploadIO.new(params['myfile'].tempfile, 'image/jpeg'),:original_filename => params['myfile'].original_filename } # this might be cool but I don't know how to use it
       
-      @response = JSON.parse(current_user.access_token.token.post('/api/v0/aspects/'+params[:activity]+'/upload', message))
+      @response = JSON.parse(current_user.access_token.token.post('/api/v0/aspects/'+params[:activity]+'/upload', message, {'Content-type'=>'multipart/form-data'}))
     
       File.open('public/images/' + params['myfile'].original_filename, "wb") do |f|
         f.write(params['myfile'].tempfile.read)
@@ -33,3 +36,4 @@ def create
   return "The file was successfully uploaded!"
 end
 end
+
