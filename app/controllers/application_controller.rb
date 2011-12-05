1aeb7074892d88d8eb835b2ebc5b731f1224b78f
Elena Nazzi <elna@itu.dk>
2011-12-05 13:41:48 +0100

    fix coffee

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   $activities = {"activity1"=>{"name"=>"petanque","value"=>"petanque"}, 
                  "activity2"=>{"name"=>"laundry","value"=>"vaskemaskiner"},
                  "activity3"=>{"name"=>"gym","value"=>"gymnastik"},
-                 #"activity4"=>{"name"=>"coffee","value"=>"kafeen"},
+                 "activity4"=>{"name"=>"coffee","value"=>"kafeen"},
                  #"activity5"=>{"name"=>"walking","value"=>"gang"},
                  "activity6"=>{"name"=>"shopping","value"=>"indk&oslash;b"}
                  }
