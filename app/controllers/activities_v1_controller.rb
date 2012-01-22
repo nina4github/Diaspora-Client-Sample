850c68e10f8b67823b9bce513d1d1faa851026e8
yijie <bingjie2680@gmail.com>
2012-01-22 15:25:42 +0100

    test api v1

--- app/controllers/activities_v1_controller.rb
+++ app/controllers/activities_v1_controller.rb
@@ -4,7 +4,7 @@ class ActivitiesV1Controller < ActionController::Base
 
     #get a users' profile
     def profile
-      @result = JSON.parse(current_user.access_token.token.get('/api/profiles/0'))
+      @result = "test"
       output(@result)
     end
 
