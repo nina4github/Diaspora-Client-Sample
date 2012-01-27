1d6394dff5ec897755dd9487ccc9e0326009f999
Yijie <bingjie2680@gmail.com>
2012-01-27 12:00:29 +0100

    test api v1

--- app/controllers/apiv1/profiles_controller.rb
+++ app/controllers/apiv1/profiles_controller.rb
@@ -2,7 +2,7 @@ class Apiv1::ProfilesController < Apiv1::BaseController
   
     #get a users' profile
     def show
-         @result = JSON.parse(current_user.access_token.token.get("/apiv1/profiles/#{params[:id]}/show"))
+         @result = JSON.parse(current_user.access_token.token.get("/api/v1/profile"))
          output(@result)
     end
     
