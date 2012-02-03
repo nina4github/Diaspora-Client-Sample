6bc5fc5e9025a49351a8d13f896f47410941752f
Yijie <bingjie2680@gmail.com>
2012-02-03 17:39:15 +0100

    update profile working

--- app/controllers/apiv1/base_controller.rb
+++ app/controllers/apiv1/base_controller.rb
@@ -25,7 +25,9 @@ class Apiv1::BaseController < ActionController::Base
             when 'delete' then
                 Net::HTTP::Delete.new(path)
         end
-        request.set_form_data(params)
+        if !params.nil?
+            request.set_form_data(params)
+        end
         return http.request(request).body
     end
 end
