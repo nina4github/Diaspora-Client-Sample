73ae32483a5f29074b4128e863e60aa7c28d599b
Yijie <bingjie2680@gmail.com>
2012-02-05 10:59:22 +0100

    update response message

--- app/controllers/apiv1/users_controller.rb
+++ app/controllers/apiv1/users_controller.rb
@@ -3,10 +3,12 @@ class Apiv1::UsersController < Apiv1::BaseController
     #create a new user
     def create
         #create a new user
-        @response = JSON.parse(forward('post', request.url, params))
+        response1 = JSON.parse(forward('post', request.url, params))
         @uri=URI.parse(request.url)
         @uri.path="/apiv1/profiles/"+params[:username]
         #update profile data
-        output(forward('put', @uri.to_s, params))
+        respons2=forward('put', @uri.to_s, params)
+        output(response1) 
+        output(response2)
     end
 end
