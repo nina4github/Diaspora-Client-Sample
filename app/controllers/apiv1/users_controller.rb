c48d70b15835626202c6ea0eddb699c16764e7d3
Yijie <bingjie2680@gmail.com>
2012-02-05 11:01:21 +0100

    update user create response message

--- app/controllers/apiv1/users_controller.rb
+++ app/controllers/apiv1/users_controller.rb
@@ -7,7 +7,7 @@ class Apiv1::UsersController < Apiv1::BaseController
         @uri=URI.parse(request.url)
         @uri.path="/apiv1/profiles/"+params[:username]
         #update profile data
-        respons2=forward('put', @uri.to_s, params)
+        response2=forward('put', @uri.to_s, params)
         output(response1) 
         output(response2)
     end
