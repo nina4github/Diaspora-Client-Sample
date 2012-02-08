624dc95595b53a9464c46cb539e23c64daf545c3
Yijie <bingjie2680@gmail.com>
2012-02-08 11:55:29 +0100

    test

--- app/controllers/apiv1/users_controller.rb
+++ app/controllers/apiv1/users_controller.rb
@@ -12,7 +12,7 @@ class Apiv1::UsersController < Apiv1::BaseController
 		
 		#create an aspect for the object
 		@uri.path="/apiv1/aspects/"
-		params["aspect"]={ 'name'=>'sharing', 'contacts_visible'=>'1' }
+		params["aspect"]={"name"=>"sharing", "contacts_visible"=>"1" }
 		output(forward('post', @uri.to_s, params))
 		#add to contact
 		if !params[:currentUser].nil?
