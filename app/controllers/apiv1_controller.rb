1357a0e4eaf3134aa0c89b257e5f09bd4c6e371e
Yijie Li <bingjie2680@gmail.com>
2012-02-27 11:30:46 +0100

    output contact ids test

--- app/controllers/apiv1_controller.rb
+++ app/controllers/apiv1_controller.rb
@@ -17,7 +17,7 @@ class Apiv1Controller < ActionController::Base
         #add contacts
         results=query('get','http://idea.itu.dk/apiv1/contacts?aspect='+params[:aspectname]+'&username='+params[:objectname]);
         contacts=ActiveSupport::JSON.decode(results)["contacts"];
-        output(results)
+        output(contacts)
     end
     
     def stream
