0e94b97a762a94e68946f7d7978f26713058b538
Yijie Li <bingjie2680@gmail.com>
2012-04-02 12:08:29 +0200

    update

--- app/controllers/apiv1/aspects_controller.rb
+++ app/controllers/apiv1/aspects_controller.rb
@@ -8,10 +8,10 @@ class Apiv1::AspectsController < Apiv1::BaseController
     def show
         result=ActiveSupport::JSON.decode(query('get',request.url))
 		aspect=Aspect.find_by_name(result[:name])
-		render :json=>{	:name=> result[:name],
-                        :id=>result[:id],
-                        :userId=>result[:user_id],
-						:feedId=>aspect.feedUrl
+		render :json=>{	:name=> result["name"],
+                        :id=> result["id"],
+                        :userId=> result["user_id"],
+						:feedId=> aspect.feedUrl
 					}
     end
     
