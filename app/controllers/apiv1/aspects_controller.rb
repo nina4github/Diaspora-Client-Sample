5e6f03ab09360491abb4cb91922eda33d627b7b1
Yijie Li <bingjie2680@gmail.com>
2012-05-14 13:19:11 +0200

    add test

--- app/controllers/apiv1/aspects_controller.rb
+++ app/controllers/apiv1/aspects_controller.rb
@@ -47,20 +47,5 @@ class Apiv1::AspectsController < Apiv1::BaseController
         @uri=URI.parse(request.url)
         @uri.path='/apiv1/aspects'
         aspect=ActiveSupport::JSON.decode(query('post',request.url, params))
-  
-        #add contacts
-        @uri.path='/apiv1/contacts'
-        @uri.query='aspect='+params[:aspectname]+'&username='+params[:objectname]
-        results=ActiveSupport::JSON.decode(query('get', @uri.to_s));
-        pids=results["pid"];
-        uids=results["uid"];
-        #add contacts to user aspect
-        @uri.query=nil
-        query('post', @uri.to_s, {:ids=>pids, :aspect=>params[:aspectname], :username=>params[:username]} );
-        #add contact to other users aspect
-        uids.each do |uid|
-            query('post', @uri.to_s, {:ids=>params[:userid], :aspect=>params[:aspectname], :userid=>uid} );
-        end
-        render :json=>{:id=>aspect["id"],"status"=>200}
     end
 end
